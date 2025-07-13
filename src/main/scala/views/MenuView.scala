f9fd3438fcf9c28edad64fe9ba0806dd7e202c85
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-07-13 21:29:23 +0200

    Modified MenuView frame location and refactor code about buttons style

--- src/main/scala/views/MenuView.scala
+++ src/main/scala/views/MenuView.scala
@@ -9,11 +9,8 @@ import javax.swing.WindowConstants.EXIT_ON_CLOSE
 
 object MenuView:
   private val frame = new JFrame("Menù")
-
-
   private val pixelFont: Font =
     Font.createFont(Font.TRUETYPE_FONT, new File("src\\main\\resources\\font\\PixelFont.ttf")).deriveFont(25f)
-
 
   private class BackgroundImagePanel(imagePath: String) extends JPanel:
     private val image: BufferedImage =
@@ -26,11 +23,31 @@ object MenuView:
       super.paintComponent(g)
       g.drawImage(image, 0,0, this)
 
+  private def setButtonStyle(text: String, size: Dimension, font: Font, backgroundColor: Color, foregroundColor: Color): JButton =
+    val button = new JButton(text)
+    button.setPreferredSize(size)
+    button.setMinimumSize(size)
+    button.setMaximumSize(size)
+    button.setFont(font)
+    button.setBackground(backgroundColor)
+    button.setForeground(foregroundColor)
+    button
 
   def show(): Unit =
-    frame.setSize(1024, 1024)
+    val screenSize = Toolkit.getDefaultToolkit.getScreenSize
+    val frameWidth = 1024
+    val frameHeight = 1024
+    val x = (screenSize.width - frameWidth) / 2
+    val y = (screenSize.height - frameHeight) / 2
+    val buttonSize = new Dimension(400, 80)
+    val bgColor = new Color(120, 180, 210)
+    val fgColor = Color.WHITE
+
+    frame.setSize(frameWidth, frameHeight)
+    frame.setLocation(x, y)
     frame.setDefaultCloseOperation(EXIT_ON_CLOSE)
     frame.setResizable(false)
+
     val backgroundPanel = new BackgroundImagePanel("src\\main\\resources\\MenuBackgroundImage.png")
     backgroundPanel.setLayout(new BorderLayout())
 
@@ -41,25 +58,8 @@ object MenuView:
     verticalButtons.setLayout(new BoxLayout(verticalButtons, BoxLayout.Y_AXIS))
     verticalButtons.setOpaque(false)
 
-    val brainAgingButton = new JButton("Age Test")
-    val brainTrainingButton = new JButton("Brain Training")
-    val buttonSize = new Dimension(400,80)
-
-    brainAgingButton.setPreferredSize(buttonSize)
-    brainAgingButton.setMinimumSize(buttonSize)
-    brainAgingButton.setMaximumSize(buttonSize)
-    brainAgingButton.setFont(pixelFont)
-    brainAgingButton.setBackground(new Color(120, 180, 210))
-    brainAgingButton.setForeground(Color.WHITE)
-
-
-    brainTrainingButton.setPreferredSize(buttonSize)
-    brainTrainingButton.setMinimumSize(buttonSize)
-    brainTrainingButton.setMaximumSize(buttonSize)
-    brainTrainingButton.setFont(pixelFont)
-    brainTrainingButton.setBackground(new Color(120, 180, 210))
-    brainTrainingButton.setForeground(Color.WHITE)
-
+    val brainAgingButton = setButtonStyle("Age Test", buttonSize, pixelFont, bgColor, fgColor)
+    val brainTrainingButton = setButtonStyle("Brain Training", buttonSize, pixelFont, bgColor, fgColor)
 
     brainAgingButton.addActionListener(_ => {
       frame.dispose()
