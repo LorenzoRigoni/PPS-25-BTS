f4fa970ecd0e1c9973072a6f116e7be8e0cb6a23
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2025-07-23 23:40:33 +0200

    Refactoring of enum parner object logic

--- src/main/scala/models/rightDirections/structure/Symbol.scala
+++ src/main/scala/models/rightDirections/structure/Symbol.scala
@@ -1,16 +1,16 @@
 package models.rightDirections.structure
 
 enum Symbol(val complexity: Int):
-  private case And extends Symbol(2)
-  private case Or extends Symbol(2)
-  private case Not extends Symbol(1)
-  private case X extends Symbol(0)
-  private case LP extends Symbol(0)
-  private case RP extends Symbol(0)
-  private case Up extends Symbol(0)
-  private case Down extends Symbol(0)
-  private case Left extends Symbol(0)
-  private case Right extends Symbol(0)
+  case And extends Symbol(2)
+  case Or extends Symbol(2)
+  case Not extends Symbol(1)
+  case X extends Symbol(0)
+  case LP extends Symbol(0)
+  case RP extends Symbol(0)
+  case Up extends Symbol(0)
+  case Down extends Symbol(0)
+  case Left extends Symbol(0)
+  case Right extends Symbol(0)
 
   override def toString: String = this match
     case And => "and"
@@ -28,17 +28,6 @@ object Symbol:
   def allOperators: List[Symbol] = List(And, Or, Not)
   def allDirections: List[Symbol] = List(Up, Left, Right, Down)
   def all: List[Symbol] = List(And, Or, Not,Up, Left, Right, Down, X,LP,RP)
-
-  def fromString(s: String): Option[Symbol] = s.toLowerCase match
-    case "and" => Some(And)
-    case "or" => Some(Or)
-    case "not"  => Some(Not)
-    case "x"  => Some(X)
-    case "up" => Some(Up)
-    case "down" => Some(Down)
-    case "left"  => Some(Left)
-    case "right"  => Some(Right)
-    case _ => None
 
   def getMaximumComplexity:Int=
     all.map(_.complexity).max
