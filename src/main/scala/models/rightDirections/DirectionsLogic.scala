0ee3db4ca0bd2908c645dc7e438d6d99431b894c
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2025-07-25 14:57:14 +0200

    DirectionLogic now works: added a function to evaluate well formed string in EvaluateOperation

--- src/main/scala/models/rightDirections/DirectionsLogic.scala
+++ src/main/scala/models/rightDirections/DirectionsLogic.scala
@@ -1,11 +1,9 @@
 package models.rightDirections
 import models.MiniGameLogic
-import models.rightDirections.structure.SyntaxTreeBuilder
+import models.rightDirections.structure.{EvaluateOperation, SyntaxTreeBuilder}
 object DirectionsLogic extends MiniGameLogic:
   override def generateQuestion(difficult: Int): String =
     SyntaxTreeBuilder.buildOperationFromComplexity(difficult).toString
 
-    
   override def validateAnswer[Direction](question: String, answer: Direction): Boolean =
-    val a = false
-    a
+    EvaluateOperation.evaluateOperationFromString(question,List()).contains(answer)
