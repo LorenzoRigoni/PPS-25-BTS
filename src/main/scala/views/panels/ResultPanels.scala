906daa96e6e72bcfa9edb1a281bd62979d04e5a3
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-07-27 15:49:41 +0200

    Refactored ResultPanels code

--- src/main/scala/views/panels/ResultPanels.scala
+++ src/main/scala/views/panels/ResultPanels.scala
@@ -29,44 +29,35 @@ sealed trait ResultPanels:
   def GameResultPanel(controller: GameController, correctAnswers: Int, wrongAnswers: Int, time: Int): JPanel
 
 class ResultPanelsImpl extends ResultPanels, BaseView:
-
-  override def TestResultPanel(controller: GameController, age: Int): JPanel =
+  private def createBaseResultPanel(controller: GameController, titleText: String): JPanel =
     val panel = new JPanel()
     panel.setLayout(new BorderLayout())
     panel.setBackground(whiteColor)
     panel.setBorder(new EmptyBorder(50, 50, 50, 50))
-
-    val title = new JLabel("Your brain age is:")
+    val title = new JLabel(titleText)
     title.setFont(pixelFont25)
     title.setHorizontalAlignment(SwingConstants.CENTER)
-
-    val ageLabel = new JLabel(age.toString)
-    ageLabel.setFont(pixelFont70)
-    ageLabel.setHorizontalAlignment(SwingConstants.CENTER)
-
     val homeButton = createStyledButton("Home", Dimension(200, 40), pixelFont15, customBlueColor, whiteColor)
     homeButton.addActionListener(_ => MenuView(controller).show())
-
     val bottomPanel = new JPanel()
     bottomPanel.setBackground(whiteColor)
     bottomPanel.add(homeButton)
-
     panel.add(title, BorderLayout.NORTH)
-    panel.add(ageLabel, BorderLayout.CENTER)
     panel.add(bottomPanel, BorderLayout.SOUTH)
+    panel
 
+  override def TestResultPanel(controller: GameController, age: Int): JPanel =
+
+    val panel = createBaseResultPanel(controller, "Your brain age is:")
+    val ageLabel = new JLabel(age.toString)
+    ageLabel.setFont(pixelFont70)
+    ageLabel.setHorizontalAlignment(SwingConstants.CENTER)
+    panel.add(ageLabel, BorderLayout.CENTER)
     panel
 
   override def GameResultPanel(controller: GameController, correctAnswers: Int, wrongAnswers: Int, time: Int): JPanel =
     val iconSize = getResponsiveIconSize(20)
-    val panel = new JPanel()
-    panel.setLayout(new BorderLayout())
-    panel.setBackground(whiteColor)
-    panel.setBorder(new EmptyBorder(50, 50, 50, 50))
-
-    val title = new JLabel("Your results:")
-    title.setFont(pixelFont25)
-    title.setHorizontalAlignment(SwingConstants.CENTER)
+    val panel = createBaseResultPanel(controller, "Your results:")
 
     def loadIcon(name: String, size: Int): ImageIcon =
       val url = getClass.getResource(s"/" + name)
@@ -77,33 +68,19 @@ class ResultPanelsImpl extends ResultPanels, BaseView:
     def resultRow(iconName: String, text: String): JPanel =
       val rowPanel = new JPanel(new FlowLayout(FlowLayout.CENTER))
       rowPanel.setBackground(whiteColor)
-
       val iconLabel = new JLabel(loadIcon(iconName, iconSize))
       val textLabel = new JLabel(text)
       textLabel.setFont(pixelFont15)
       rowPanel.add(iconLabel)
       rowPanel.add(Box.createRigidArea(new Dimension(10, 0)))
       rowPanel.add(textLabel)
-
       rowPanel
 
     val resultsPanel = new JPanel()
     resultsPanel.setBackground(whiteColor)
     resultsPanel.setLayout(new GridLayout(3, 1, 10, 20))
-
     resultsPanel.add(resultRow("greenCheck.png", s"Correct Answers: $correctAnswers"))
     resultsPanel.add(resultRow("redCross.png", s"Wrong Answers: $wrongAnswers"))
     resultsPanel.add(resultRow("clock.png", s"Time: $time seconds"))
-
-    val homeButton = createStyledButton("Home", Dimension(200, 40), pixelFont15, customBlueColor, whiteColor)
-    homeButton.addActionListener(_  => MenuView(controller).show())
-
-    val bottomPanel = new JPanel()
-    bottomPanel.setBackground(whiteColor)
-    bottomPanel.add(homeButton)
-
-    panel.add(title, BorderLayout.NORTH)
     panel.add(resultsPanel, BorderLayout.CENTER)
-    panel.add(bottomPanel, BorderLayout.SOUTH)
-
     panel
