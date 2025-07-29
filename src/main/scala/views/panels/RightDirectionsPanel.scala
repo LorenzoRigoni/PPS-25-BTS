320af4364f15c7985fdd6b1d2a0e8ff1c2cf2fd0
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2025-07-29 11:48:22 +0200

    Implemented correctly the view of Right Directions

--- src/main/scala/views/panels/RightDirectionsPanel.scala
+++ src/main/scala/views/panels/RightDirectionsPanel.scala
@@ -4,21 +4,45 @@ import controllers.GameController
 
 import javax.swing.*
 import java.awt.*
+import java.awt.event.{KeyAdapter, KeyEvent}
 
-/**
- * This objects represents the view of the mini-game Right Directions.
- */
 class RightDirectionsPanel(controller: GameController, onNext: GameController => Unit)
-    extends SimpleQuestionAnswerGamePanel:
+  extends SimpleQuestionAnswerGamePanel:
+
   def panel(): JPanel =
-    createSimpleQuestionAnswerGamePanel(
+
+    def validateAnswer(input: String): (String, Color) =
+      if controller.checkAnswer(input) then
+        val increased = controller.increaseDifficulty()
+        onNext(increased)
+        ("Correct!", Color.GREEN)
+      else
+        ("Wrong!", Color.RED)
+
+    val panel = createSimpleQuestionAnswerGamePanel(
       question = controller.lastQuestion.get,
       textInputLabel = "Your answer: ",
-      validate = input =>
-        if controller.checkAnswer(input) then
-          val increased = controller.increaseDifficulty()
-          onNext(increased)
-          ("Correct!", Color.GREEN)
-        else (s"Wrong!", Color.RED),
+      validate = validateAnswer,
       controller = controller
     )
+
+    panel.addKeyListener(new KeyAdapter {
+      override def keyPressed(e: KeyEvent): Unit = {
+        val answerOpt: Option[String] = e.getKeyCode match
+          case KeyEvent.VK_W => Some("up")
+          case KeyEvent.VK_A => Some("left")
+          case KeyEvent.VK_S => Some("down")
+          case KeyEvent.VK_D => Some("right")
+          case KeyEvent.VK_J => Some("")
+          case _ => None
+
+          answerOpt.foreach { answer =>
+            val (message, color) = validateAnswer(answer)
+          }
+      }
+    })
+
+    panel.setFocusable(true)
+    SwingUtilities.invokeLater(() => panel.requestFocusInWindow())
+
+    panel
