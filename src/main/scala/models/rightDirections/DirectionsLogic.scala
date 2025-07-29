759d668ab6fd81d9d33c4e4b0af86c828a02819e
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2025-07-29 11:46:25 +0200

    Renamed RightDirections for consistency

--- src/main/scala/models/rightDirections/DirectionsLogic.scala
+++
@@ -1,9 +0,0 @@
-package models.rightDirections
-import models.MiniGameLogic
-import models.rightDirections.structure.{EvaluateOperation, SyntaxTreeBuilder}
-object DirectionsLogic extends MiniGameLogic:
-  override def generateQuestion(difficult: Int): String =
-    SyntaxTreeBuilder.buildOperationFromComplexity(difficult).toString
-
-  override def validateAnswer[Direction](question: String, answer: Direction): Boolean =
-    EvaluateOperation.evaluateOperationFromString(question,List()).contains(answer)
