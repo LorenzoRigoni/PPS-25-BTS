5b94396b319ce7b3a1d773a43d8de22edb56cfab
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2025-08-18 06:56:25 +0200

    old tests

--- src/test/scala/models/rightDirections/RightDirectionsTests.scala
+++ src/test/scala/models/rightDirections/RightDirectionsTests.scala
@@ -1,30 +1,43 @@
 package models.rightDirections
 
-import models.rightDirections.structure.Symbol.{And, Not, Or, X}
+import models.rightDirections.structure.Token
 import models.rightDirections.structure.treeLogic.{BinaryTree, Leaf, Node}
-import models.rightDirections.structure.{Symbol, *}
 import org.scalatest.funsuite.AnyFunSuite
+import models.rightDirections.structure.EvaluateOperation.evaluateOperationFromString as evaluate
+import models.rightDirections.structure.Token.Up
 
 class RightDirectionsTests extends AnyFunSuite:
-  val tree: BinaryTree[Symbol] = new Leaf(X).expand(X, Symbol.Left, Option(Not), None)
+  val operationWithResults    = new Node(Token.Or, new Leaf(Token.Up), Option(new Leaf(Token.Right)))
+  val operationWithoutResults =
+    new Node(Token.And, new Leaf(Token.Up), Option(new Leaf(Token.Right)))
 
-  test("Check Not operator") {
-    // EvaluateOperation.evaluateOperationFromString("(((((right or right) not) or (left and up)) not) not)",List())
-    // SyntaxTreeBuilder.expandTree(new Node(Symbol.Not,new Leaf(Symbol.X),None),5)
-    // assert(tree.depth == 5)
-    // assert(List(Symbol.Up,Symbol.Right,Symbol.Down).equals(EvaluateOperation.evaluateOperationFromTree(tree)))
+  val operationWith1Not  = new Node(Token.Not, new Leaf(Token.Up), None)
+  val operationWith2Not = new Node(Token.Not, operationWith1Not, None)
+  val operationWith3Not = new Node(Token.Not, operationWith2Not, None)
+
+  val operationWith1Result               = new Leaf(Token.Up)
+  val operationWith2Results: Node[Token] = operationWithResults
+  val operationWith3Results: Node[Token] = operationWith1Not
+  val operationWith4Results              = new Node(Token.Or, operationWith1Not, Option(operationWith1Result))
+
+  test("Check if the evaluation of functions works correctly") {
+    assert(evaluate(operationWithResults.toString, List()).nonEmpty)
+    assert(evaluate(operationWithoutResults.toString, List()).isEmpty)
   }
-  test("Check more binary operators") {
-    // val andTreeIncorrect = new Node(Symbol.And,tree,Option(new Leaf(Symbol.Left)))
-    // val andTreeCorrect = new Node(Symbol.And,tree,Option(new Leaf(Symbol.Right)))
 
-    // assert(List().empty.equals(EvaluateOperation.evaluateOperationFromTree(andTreeIncorrect)))
-    // assert(List(Symbol.Right).equals(EvaluateOperation.evaluateOperationFromTree(andTreeCorrect)))
+  test("Check if not gets correctly evaluated") {
+    val oneNotResult: Seq[Token] = evaluate(operationWith1Not.toString, List())
+    val twoNotResult: Seq[Token] = evaluate(operationWith2Not.toString, List())
+    val threeNotResult: Seq[Token] = evaluate(operationWith3Not.toString, List())
+    
+    assert(oneNotResult.size == 3)
+    assert(twoNotResult.size == 1 && twoNotResult.contains(Up))
+    assert(oneNotResult equals threeNotResult)
   }
 
-  test("Check operation evaluation") {
-    val complexTree = SyntaxTreeBuilder.buildOperationFromComplexity(9)
-    val stringTree  = complexTree.toString
-    val result      = EvaluateOperation.evaluateOperationFromString(stringTree, List())
-    print(stringTree, result)
+  test("check if the correct amount of directions are returned") {
+    assert(evaluate(operationWith1Result.toString, List()).size == 1)
+    assert(evaluate(operationWith2Results.toString, List()).size == 2)
+    assert(evaluate(operationWith3Results.toString, List()).size == 3)
+    assert(evaluate(operationWith4Results.toString, List()).size == 4)
   }
