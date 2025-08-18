487fc352535c3b48f0f936e4822a6f649d7391e6
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-08-18 10:47:08 +0200

    Refactor FastCalcTests and WordMemoryTests using matchers

--- src/test/scala/models/FastCalcTests.scala
+++ src/test/scala/models/FastCalcTests.scala
@@ -3,11 +3,12 @@ package models
 import org.scalatest.funsuite.AnyFunSuite
 import utils.constants.FastCalcConstants.{FAST_CALC_DIFFICULTY_STEP, FAST_CALC_TURNS}
 import utils.SimpleTextQuestion
+import org.scalatest.matchers.should.Matchers
 
 /**
  * This class tests the logic of the mini-game Fast Calc.
  */
-class FastCalcTests extends AnyFunSuite:
+class FastCalcTests extends AnyFunSuite with Matchers:
   private val TEST_DIFFICULTY_INDEX           = 3
   private val TEST_EXPRESSION_POSITIVE_RESULT = SimpleTextQuestion("8 + 4 * 2")
   private val logic                           = FastCalcLogic(
@@ -27,57 +28,53 @@ class FastCalcTests extends AnyFunSuite:
   test(
     "The validator of the mini-game should return true for the correct answers (case positive results)"
   ) {
-    assert(logic.validateAnswer(CORRECT_ANSWER1))
+    logic.validateAnswer(CORRECT_ANSWER1) shouldBe true
   }
 
   test(
     "The validator of the mini-game should return false for the wrong answers (case positive results)"
   ) {
-    assert(!logic.validateAnswer(WRONG_ANSWER1))
+    logic.validateAnswer(WRONG_ANSWER1) shouldBe false
   }
 
   test(
     "The validator of the mini-game should return true for the correct answers (case negative results)"
   ) {
-    assert(logic2.validateAnswer(CORRECT_ANSWER2))
+    logic2.validateAnswer(CORRECT_ANSWER2) shouldBe true
   }
 
   test(
     "The validator of the mini-game should return false for the wrong answers (case negative results)"
   ) {
-    assert(!logic2.validateAnswer(WRONG_ANSWER2))
+    logic2.validateAnswer(WRONG_ANSWER2) shouldBe false
   }
 
   test(
     "The method calculate answer should be able to calculate expressions with positive result"
   ) {
-    assert(CORRECT_ANSWER1 == 16)
+    CORRECT_ANSWER1 shouldBe 16
   }
 
   test(
     "The method calculate answer should be able to calculate expressions with negative result"
   ) {
-    assert(CORRECT_ANSWER2 == -1)
+    CORRECT_ANSWER2 shouldBe -1
   }
 
   test(
     "The method generateQuestion should return a non-empty string and new logic with difficulty increased"
   ) {
     val (newLogic, question) = logic.generateQuestion
-    assert(question.text.nonEmpty)
-    assert(
-      newLogic
-        .asInstanceOf[FastCalcLogic]
-        .difficulty == logic.difficulty + FAST_CALC_DIFFICULTY_STEP
-    )
+    question.text should not be empty
+    newLogic.asInstanceOf[FastCalcLogic].difficulty shouldBe logic.difficulty + FAST_CALC_DIFFICULTY_STEP
   }
 
   test("The method isMiniGameFinished should return false until rounds are reached") {
     val (nextLogic, _) = logic.generateQuestion
-    assert(!nextLogic.isMiniGameFinished)
+    nextLogic.isMiniGameFinished shouldBe false
   }
 
   test("The method isMiniGameFinished should return true when rounds are finished") {
     val finishedLogic = FastCalcLogic(rounds = 1, currentRound = 1)
-    assert(finishedLogic.isMiniGameFinished)
+    finishedLogic.isMiniGameFinished shouldBe true
   }
